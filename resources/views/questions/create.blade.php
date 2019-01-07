dc1301e408cfcf9c79a63c6d40f6d4c1ba8f0560
Sampuccino <Sampuccino@gmail.com>
2019-01-06 17:07:38 -0800

    Built out question form.

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -17,14 +17,28 @@
                     <div class="card-body">
 
                         <form action="{{route('questions.store')}}" method="post">
+
                             <div class="form-group">
                                 <label for="question-title">Question title</label>
-                                <input type="text" name="title" id="question-title" class="form-control">
+                                <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+
+                                @if ($errors->has('title'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $errors->first('title')  }}</strong>
+                                    </div>
+                                @endif
                             </div>
 
                             <div class="form-group">
                                 <label for="question-body">Question explanation</label>
-                                <textarea name="body" id="question-body" cols="30" rows="10" class="form-control"></textarea>
+                                <textarea name="body" id="question-body" cols="30" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+
+                                @if ($errors->has('body'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{ $errors->first('body')  }}</strong>
+                                    </div>
+                                @endif
+
                             </div>
 
                             <div class="form-group">
