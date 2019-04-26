c5311463c977941734afb61742dbb3f177dcf86c
Sampuccino <Sampuccino@gmail.com>
2019-04-26 13:51:44 -0700

    Implemented Authorization using Gates.

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,13 +37,17 @@
                                             <a href="{{ $question->url }}">{{$question->title}}</a>
                                         </h3>
                                       <div class="ml-auto">
-                                          <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-success">Edit</a>
+                                          @if (Auth::user()->can('update-question', $question))
+                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-success">Edit</a>
+                                          @endif
                                       </div>
-                                        <form action="{{route('questions.destroy', $question->id)}}" method="post">
-                                            @method('DELETE')
-                                            @csrf {{--  Blade directive token--}}
-                                            <button type="submit" class="btn btn-danger btn-sm ml-1" onclick="return confirm('Are you sure?')">X</button>
-                                        </form>
+                                        @if (Auth::user()->can('delete-question', $question))
+                                          <form action="{{route('questions.destroy', $question->id)}}" method="post">
+                                              @method('DELETE')
+                                              @csrf {{--  Blade directive token--}}
+                                              <button type="submit" class="btn btn-danger btn-sm ml-1" onclick="return confirm('Are you sure?')">X</button>
+                                          </form>
+                                        @endif
 
                                     </div>
                                     <p class="lead">
