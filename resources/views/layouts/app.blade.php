546d11b9bf1441cf5ddaec0961f2a9f7720fd08e
Sampuccino <Sampuccino@gmail.com>
2019-05-06 22:19:31 -0700

    Added fontawesome.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -4,35 +4,54 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card-title">
                     <div class="d-flex align-items-center">
-                        <h1>{{ $question->title }}</h1>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index') }}" class="btn btn-primary">Back</a>
-                        </div>
+                      <h1>{{ $question->title }}</h1>
+                      <div class="ml-auto">
+                        <a href="{{ route('questions.index') }}" class="btn btn-primary">Back</a>
+                      </div>
                     </div>
+                  </div>
+                  <hr>
+                  <div class="media">
+
+                    <div class="d-flex flex-column vote-controls">
+                      <a title="This question is useful" class="vote-up">
+                        <i class="fa fa-caret-up" aria-hidden="true"></i>
+                      </a>
+                      <span class="votes-count">1230</span>
+                      <a title="This question was terrible" class="vote-down off">
+                        <i class="fa fa-caret-down" aria-hidden="true"></i>
+                      </a>
+                      <a title="Clickto mark as favorite question (Click again to undo)" class="favorite">
+                        Favorite
+                        <i class="fa fa-star" aria-hidden="true"></i>
+                      </a>
+
                     </div>
 
-                    <div class="card-body">
+                    <div class="media-body">
+                      {!! $question->body_html !!}
+                      <div class="float-right">
+                        <span class="text-muted">Created By {{ $question->created_date }}</span>
+                        <div class="media">
+                          <a href="{{ $question->user->url }}" class="pr-2">
+                            <img src="{{ $question->user->avatar }}" alt="">
+                          </a>
 
-                        {!! $question->body_html !!}
-                        <div class="float-right">
-                          <span class="text-muted">Created By {{ $question->created_date }}</span>
-                          <div class="media">
-                            <a href="{{ $question->user->url }}" class="pr-2">
-                              <img src="{{ $question->user->avatar }}" alt="">
-                            </a>
-
-                            <div class="media-body">
-                              <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                            </div>
-
+                          <div class="media-body">
+                            <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                           </div>
 
+                        </div>
+
                       </div>
+                    </div>
+                  </div>
                 </div>
-            </div>
+              </div>
         </div>
         <div class="row mt-4">
           <div class="col-md-12">
