97e6cdf5d0d4c2b0cbdfb70aa95a25300bc78047
Sampuccino <Sampuccino@gmail.com>
2019-05-24 23:51:01 -0700

    Updated Favoriting a question as a user.

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -18,17 +18,27 @@
                   <div class="media">
 
                     <div class="d-flex flex-column vote-controls">
-                      <a title="This question is useful" class="vote-up">
+                      <a title="This question is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}">
                         <i class="fa fa-caret-up"></i>
                       </a>
                       <span class="votes-count">1230</span>
                       <a title="This question was terrible" class="vote-down off">
                         <i class="fa fa-caret-down"></i>
                       </a>
-                      <a title="Clickto mark as favorite question (Click again to undo)" class="favorite mt-2 favorited">
+                      <a title="Click to mark as favorite question (Click again to undo)"
+                         class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }} "
+                         onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit()"
+                        >
                         <i class="fa fa-star"></i>
                         <span class="favorites-count">{{ $question->favorites_count }}</span>
                       </a>
+
+                      <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="POST" style="display: none">
+                        @csrf
+                        @if ($question->is_favorited)
+                          @method('DELETE')
+                        @endif
+                      </form>
 
                     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,8 @@ Route::resource('questions.answers', 'AnswersController')->except(['index', 'cre
 /* Gets question by SLUG or title: Ex Day-One-Que for better SEO */
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
+
+Route::post('/questions/{question}/favorites', 'FavoritesController@store')->name('questions.favorite');
+Route::delete('/questions/{question}/favorites', 'FavoritesController@destroy')->name('questions.unfavorite');
+
+Route::post('/questions/{question}/vote', 'VoteQuestionController');
